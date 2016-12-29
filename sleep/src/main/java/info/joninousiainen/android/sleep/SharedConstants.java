4bb895c4ed599e37e9d2d2a199e0f0c93d836a1c
Joni Nousiainen <developer@joninousiainen.info>
2016-12-29 23:06:22 +0200

    Added statistics from last 30 days, work in progress.

--- sleep/src/main/java/info/joninousiainen/android/sleep/SharedConstants.java
+++ sleep/src/main/java/info/joninousiainen/android/sleep/SharedConstants.java
@@ -3,6 +3,9 @@ package info.joninousiainen.android.sleep;
 public final class SharedConstants {
     public static final String PREFERENCES_FILE_NAME = "info.joninousiainen.android.sleep.Preferences";
     public static final String ANSWER_KEY_PREFIX = "answer-";
+    public static final String ANSWER_YES = "yes";
+    public static final String ANSWER_NO = "no";
+    public static final String ANSWER_EMPTY = "no_answer";
     public static final String DATE_FORMAT = "yyyyMMdd";
 
     private SharedConstants() {}
--- sleep/src/main/java/info/joninousiainen/android/sleep/ViewHistoryActivity.java
+++ sleep/src/main/java/info/joninousiainen/android/sleep/ViewHistoryActivity.java
@@ -1,9 +1,9 @@
 package info.joninousiainen.android.sleep;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -28,34 +28,25 @@ public class ViewHistoryActivity extends AppCompatActivity {
 
         addDatesWhichHaveNoAnswer(answersByDate);
 
-        Calendar calendar = Calendar.getInstance();
+        addLast30DaysStatistics(answersByDate);
+
         DateFormat dateFormat = new SimpleDateFormat(getText(R.string.history_date_format).toString());
-        LinearLayout layout = (LinearLayout) findViewById(R.id.viewHistoryMainLayout);
+        LinearLayout allAnswersLayout = (LinearLayout) findViewById(R.id.allAnswersLayout);
+        Calendar calendar = Calendar.getInstance();
 
         for (Map.Entry<Date, String> entry : answersByDate.entrySet()) {
             Date date = entry.getKey();
-
-            CharSequence localizedValue;
-            switch (entry.getValue()) {
-                case "yes":
-                    localizedValue = getText(R.string.history_answer_yes);
-                    break;
-                case "no":
-                    localizedValue = getText(R.string.history_answer_no);
-                    break;
-                default:
-                    localizedValue = getText(R.string.history_answer_empty);
-            }
+            String answer = entry.getValue();
 
             TextView text = new TextView(this);
-            text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-            text.setText(dateFormat.format(date) + ": " + localizedValue);
-            layout.addView(text);
+            text.setTextAppearance(android.R.style.TextAppearance_Medium);
+            text.setText(dateFormat.format(date) + ": " + getLocalizedAnswer(answer));
+            allAnswersLayout.addView(text);
 
             // Add ruler between weeks
             calendar.setTime(date);
             if (calendar.getFirstDayOfWeek() == calendar.get(Calendar.DAY_OF_WEEK)) {
-                addRuler(layout);
+                addRuler(allAnswersLayout);
             }
         }
     }
@@ -74,7 +65,7 @@ public class ViewHistoryActivity extends AppCompatActivity {
                     throw new RuntimeException("Date has been stored in wrong format: " + dateString, e);
                 }
 
-                String answer = preferences.getString(key, "");
+                String answer = preferences.getString(key, SharedConstants.ANSWER_EMPTY);
 
                 sortedDates.put(date, answer);
             }
@@ -89,7 +80,7 @@ public class ViewHistoryActivity extends AppCompatActivity {
             Date date = calendar.getTime();
             String value = answersByDate.get(date);
             if (value == null) {
-                answersByDate.put(date, "");
+                answersByDate.put(date, SharedConstants.ANSWER_EMPTY);
             }
             calendar.add(Calendar.DAY_OF_MONTH, -1);
         }
@@ -111,6 +102,50 @@ public class ViewHistoryActivity extends AppCompatActivity {
             lastEntry = iterator.next();
         }
         return lastEntry.getKey();
+    }
+
+    private void addLast30DaysStatistics(Map<Date, String> answersByDate) {
+        // TODO: Don't show this when oldest answer is less than 30 days old.
+
+        Map<String, Integer> answerCounts = new LinkedHashMap<>();
+        answerCounts.put(SharedConstants.ANSWER_YES, 0);
+        answerCounts.put(SharedConstants.ANSWER_NO, 0);
+        answerCounts.put(SharedConstants.ANSWER_EMPTY, 0);
+        int counter = 0;
+        for (Map.Entry<Date, String> entry : answersByDate.entrySet()) {
+            String answer = entry.getValue();
+            Integer count = answerCounts.get(answer);
+            answerCounts.put(answer, count + 1);
+
+            if (counter > 30) {
+                break;
+            }
+            else {
+                counter++;
+            }
+        }
+        LinearLayout last30DaysLayout = (LinearLayout) findViewById(R.id.last30DaysLayout);
+        for (Map.Entry<String, Integer> entry : answerCounts.entrySet()) {
+            TextView text = new TextView(this);
+            text.setTextAppearance(android.R.style.TextAppearance_Medium);
+            text.setText(getLocalizedAnswer(entry.getKey()) + ": " + entry.getValue());
+            last30DaysLayout.addView(text);
+        }
+    }
+
+    private CharSequence getLocalizedAnswer(String answer) {
+        CharSequence localizedAnswer;
+        switch (answer) {
+            case SharedConstants.ANSWER_YES:
+                localizedAnswer = getText(R.string.history_answer_yes);
+                break;
+            case SharedConstants.ANSWER_NO:
+                localizedAnswer = getText(R.string.history_answer_no);
+                break;
+            default:
+                localizedAnswer = getText(R.string.history_answer_empty);
+        }
+        return localizedAnswer;
     }
 
     private void addRuler(LinearLayout layout) {
